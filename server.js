766a1eb6c0ff47c4279ffdde1ab0fd9172977eed
Rishabh Saxena <orishabh1@gmail.com>
2015-08-19 11:54:40 +0530

    Auth Route protection WIP

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ var port = process.env.PORT || 8080; // set our port
 // Configuring Passport
 var passport = require('passport');
 var expressSession = require('express-session');
-app.use(expressSession({secret: 'mySecretKey'}));
+var LocalStrategy = require('passport-local').Strategy;
+app.use(expressSession({secret: 'mySecretKey',  resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -121,150 +122,73 @@ function(req, username, password, done) {
   process.nextTick(findOrCreateUser);
 }));
 
-            // Generates hash using bCrypt
-            var createHash = function(password){
-              return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-            }
+// Generates hash using bCrypt
+var createHash = function(password){
+  return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+}
 
-            // ROUTES FOR OUR API
-            // =============================================================================
-            var router = express.Router(); // get an instance of the express Router
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router(); // get an instance of the express Router
 
-            // middleware to use for all requests
-            router.use(function(req, res, next) {
-              // do logging
-              console.log('Something is happening.');
-              res.header("Access-Control-Allow-Origin", "*");
-              res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
-              res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-              if ('OPTIONS' == req.method) {
-                return res.sendStatus(200);
-              }
-              res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-              next(); // make sure we go to the next routes and don't stop here
-            });
+// middleware to use for all requests
+router.use(function(req, res, next) {
+  // do logging
+  console.log('Something is happening.');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  if ('OPTIONS' == req.method) {
+    return res.sendStatus(200);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next(); // make sure we go to the next routes and don't stop here
+});
 
-            /* GET login page. */
-            router.get('/', function(req, res) {
-              // Display the Login page with any flash message, if any
-              res.render('index', { message: req.flash('message') });
-            });
+/* GET login page. */
+router.get('/', function(req, res) {
+  // Display the Login page with any flash message, if any
+  res.render('index', { message: req.flash('message') });
+});
 
-            /* Handle Login POST */
-            router.post('/login', passport.authenticate('login', {
-              successRedirect: '/home',
-              failureRedirect: '/',
-              failureFlash : true
-            }));
+/* Handle Login POST */
+router.post('/login', passport.authenticate('login', {
+  successRedirect: '/home',
+  failureRedirect: '/',
+  failureFlash : true
+}));
 
-            /* GET Registration Page */
-            router.get('/signup', function(req, res){
-              res.render('register',{message: req.flash('message')});
-            });
+/* GET Registration Page */
+router.get('/signup', function(req, res){
+  res.render('register',{message: req.flash('message')});
+});
 
-            /* Handle Registration POST */
-            router.post('/signup', passport.authenticate('signup', {
-              successRedirect: '/home',
-              failureRedirect: '/signup',
-              failureFlash : true
-            }));
+/* Handle Registration POST */
+router.post('/signup', passport.authenticate('signup', {
+  successRedirect: '/home',
+  failureRedirect: '/signup',
+  failureFlash : true
+}));
 
-            // more routes for our API will happen here
+/* Handle Logout */
+router.get('/signout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
-            // on routes that end in /events
-            // ----------------------------------------------------
-
-
-            router.route('/events')
-
-            // create a event (accessed at POST http://localhost:8080/api/events)
-            .post(function(req, res) {
-              console.log(req.body);
-
-              var event = new Event(); // create a new instance of the Event model
-              event.eventName = req.body.eventName; // set the events name (comes from the request)
-              event.Time = req.body.Time;
-              event.fromDate = req.body.fromDate;
-              event.untilDate = req.body.untilDate;
-              event.Description = req.body.Description;
-              event.recurrence = req.body.recurrence;
-              event.eventListName = req.body.mailingListName;
-
-              // save the event and check for errors
-              event.save(function(err) {
-                if (err)
-                  res.send(err);
-
-                res.json({
-                  message: 'Event created!'
-                });
-              });
-            })
-            .get(function(req, res) {
-              Event.find(function(err, events) {
-                if (err)
-                  res.send(err);
-
-                res.json(events);
-              });
-            });
-
-            // on routes that end in /events/:event_id
-            // ----------------------------------------------------
-            router.route('/events/:event_id')
-
-            // get the event with that id (accessed at GET http://localhost:8080/api/events/:event_id)
-            .get(function(req, res) {
-              Event.findById(req.params.event_id, function(err, event) {
-                if (err)
-                  res.send(err);
-                res.json(event);
-              });
-            })
-
-            // update the event with this id (accessed at PUT http://localhost:8080/api/events/:event_id)
-            .put(function(req, res) {
-
-              // use our event model to find the event we want
-              Event.findById(req.params.event_id, function(err, event) {
-
-                if (err)
-                  res.send(err);
-
-                event.eventName = req.body.eventName; // set the events name (comes from the request)
-                event.Time = req.body.Time;
-                event.fromDate = req.body.fromDate;
-                event.untilDate = req.body.untilDate;
-                event.Description = req.body.Description;
-                event.recurrence = req.body.recurrence;
-                event.eventListName = req.body.mailingListName;   // update the events info
-
-                // save the event
-                event.save(function(err) {
-                  if (err)
-                    res.send(err);
-
-                  res.json({
-                    message: 'Event updated!'
-                  });
-                });
-
-              });
-            })
-
-            // delete the event with this id (accessed at DELETE http://localhost:8080/api/events/:event_id)
-            .delete(function(req, res) {
-              Event.remove({
-                _id: req.params.event_id
-              }, function(err, event) {
-                if (err)
-                  res.send(err);
-
-                res.json({
-                  message: 'Successfully deleted'
-                });
-              });
-            });
+/* GET Home Page */
+/*
+router.get('/home', isAuthenticated, function(req, res){
+  res.render('home', { user: req.user });
+});*/
+ 
+// As with any middleware it is quintessential to call next()
+// if the user is authenticated
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/');
+}
 
 
 
@@ -272,97 +196,198 @@ function(req, username, password, done) {
 
 
 
+// more routes for our API will happen here
+
+// on routes that end in /events
+// ----------------------------------------------------
+router.route('/events')
+
+// create a event (accessed at POST http://localhost:8080/api/events)
+.post(function(req, res) {
+  console.log(req.body);
+
+  var event = new Event(); // create a new instance of the Event model
+  event.eventName = req.body.eventName; // set the events name (comes from the request)
+  event.Time = req.body.Time;
+  event.fromDate = req.body.fromDate;
+  event.untilDate = req.body.untilDate;
+  event.Description = req.body.Description;
+  event.recurrence = req.body.recurrence;
+  event.eventListName = req.body.mailingListName;
+
+  // save the event and check for errors
+  event.save(function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({
+      message: 'Event created!'
+    });
+  });
+})
+.get(function(req, res) {
+  Event.find(function(err, events) {
+    if (err)
+      res.send(err);
+
+    res.json(events);
+  });
+});
+
+// on routes that end in /events/:event_id
+// ----------------------------------------------------
+router.route('/events/:event_id')
+
+// get the event with that id (accessed at GET http://localhost:8080/api/events/:event_id)
+.get(function(req, res) {
+  Event.findById(req.params.event_id, function(err, event) {
+    if (err)
+      res.send(err);
+    res.json(event);
+  });
+})
+
+// update the event with this id (accessed at PUT http://localhost:8080/api/events/:event_id)
+.put(function(req, res) {
+
+  // use our event model to find the event we want
+  Event.findById(req.params.event_id, function(err, event) {
+
+    if (err)
+      res.send(err);
+
+    event.eventName = req.body.eventName; // set the events name (comes from the request)
+    event.Time = req.body.Time;
+    event.fromDate = req.body.fromDate;
+    event.untilDate = req.body.untilDate;
+    event.Description = req.body.Description;
+    event.recurrence = req.body.recurrence;
+    event.eventListName = req.body.mailingListName;   // update the events info
+
+    // save the event
+    event.save(function(err) {
+      if (err)
+        res.send(err);
+
+      res.json({
+        message: 'Event updated!'
+      });
+    });
+
+  });
+})
+
+// delete the event with this id (accessed at DELETE http://localhost:8080/api/events/:event_id)
+.delete(function(req, res) {
+  Event.remove({
+    _id: req.params.event_id
+  }, function(err, event) {
+    if (err)
+      res.send(err);
+
+    res.json({
+      message: 'Successfully deleted'
+    });
+  });
+});
 
 
-            // on routes that end in /mailingLists
-            // ----------------------------------------------------
 
 
-            router.route('/mailingLists')
-
-            // create a mailingList (accessed at POST http://localhost:8080/api/mailingLists)
-            .post(function(req, res) {
-              console.log(req.body);
-
-              var mailingList = new MailingList(); // create a new instance of the mailingList model
-              mailingList.name = req.body.name;
-
-              // save the mailingList and check for errors
-              mailingList.save(function(err) {
-                if (err)
-                  res.send(err);
-
-                res.json({
-                  message: 'mailingList created!'
-                });
-              });
-            })
-            .get(function(req, res) {
-              MailingList.find(function(err, mailingLists) {
-                if (err)
-                  res.send(err);
-
-                res.json(mailingLists);
-              });
-            });
-
-            // on routes that end in /mailingLists/:mailingList_id
-            // ----------------------------------------------------
-            router.route('/mailingLists/:mailingList_id')
-
-            // get the mailingList with that id (accessed at GET http://localhost:8080/api/mailingLists/:mailingList_id)
-            .get(function(req, res) {
-              MailingList.findById(req.params.mailingList_id, function(err, mailingList) {
-                if (err)
-                  res.send(err);
-                res.json(mailingList);
-              });
-            })
-
-            // update the mailingList with this id (accessed at PUT http://localhost:8080/api/mailingLists/:mailingList_id)
-            .put(function(req, res) {
-
-              // use our mailingList model to find the mailingList we want
-              MailingList.findById(req.params.mailingList_id, function(err, mailingList) {
-
-                if (err)
-                  res.send(err);
-
-                mailingList.name = req.body.name;
-
-                // save the mailingList
-                mailingList.save(function(err) {
-                  if (err)
-                    res.send(err);
-
-                  res.json({
-                    message: 'mailingList updated!'
-                  });
-                });
-
-              });
-            })
-
-            // delete the mailingList with this id (accessed at DELETE http://localhost:8080/api/mailingLists/:mailingList_id)
-            .delete(function(req, res) {
-              MailingList.remove({
-                _id: req.params.mailingList_id
-              }, function(err, mailingList) {
-                if (err)
-                  res.send(err);
-
-                res.json({
-                  message: 'Successfully deleted'
-                });
-              });
-            });
 
 
-            // REGISTER OUR ROUTES -------------------------------
-            // all of our routes will be prefixed with /api
-            app.use('/api', router);
 
-            // START THE SERVER
-            // =============================================================================
-            app.listen(port);
-            console.log('Magic happens on port ' + port);
+
+
+// on routes that end in /mailingLists
+// ----------------------------------------------------
+
+
+router.route('/mailingLists')
+
+// create a mailingList (accessed at POST http://localhost:8080/api/mailingLists)
+.post(function(req, res) {
+  console.log(req.body);
+
+  var mailingList = new MailingList(); // create a new instance of the mailingList model
+  mailingList.name = req.body.name;
+
+  // save the mailingList and check for errors
+  mailingList.save(function(err) {
+    if (err)
+      res.send(err);
+
+    res.json({
+      message: 'mailingList created!'
+    });
+  });
+})
+.get(function(req, res) {
+  MailingList.find(function(err, mailingLists) {
+    if (err)
+      res.send(err);
+
+    res.json(mailingLists);
+  });
+});
+
+// on routes that end in /mailingLists/:mailingList_id
+// ----------------------------------------------------
+router.route('/mailingLists/:mailingList_id')
+
+// get the mailingList with that id (accessed at GET http://localhost:8080/api/mailingLists/:mailingList_id)
+.get(function(req, res) {
+  MailingList.findById(req.params.mailingList_id, function(err, mailingList) {
+    if (err)
+      res.send(err);
+    res.json(mailingList);
+  });
+})
+
+// update the mailingList with this id (accessed at PUT http://localhost:8080/api/mailingLists/:mailingList_id)
+.put(function(req, res) {
+
+  // use our mailingList model to find the mailingList we want
+  MailingList.findById(req.params.mailingList_id, function(err, mailingList) {
+
+    if (err)
+      res.send(err);
+
+    mailingList.name = req.body.name;
+
+    // save the mailingList
+    mailingList.save(function(err) {
+      if (err)
+        res.send(err);
+
+      res.json({
+        message: 'mailingList updated!'
+      });
+    });
+
+  });
+})
+
+// delete the mailingList with this id (accessed at DELETE http://localhost:8080/api/mailingLists/:mailingList_id)
+.delete(function(req, res) {
+  MailingList.remove({
+    _id: req.params.mailingList_id
+  }, function(err, mailingList) {
+    if (err)
+      res.send(err);
+
+    res.json({
+      message: 'Successfully deleted'
+    });
+  });
+});
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
